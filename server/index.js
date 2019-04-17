9bcd4eec319240a7d1f3286dd0d3019e24266ce8
DAGIKE <isaac20053@hotmail.com>
2019-04-17 12:14:55 -0400

    implement server user authentication

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
-require("./services/passport");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 require("./models/user");
+require("./services/passport");
 const keys = require("./config/keys");
 
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
 
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
