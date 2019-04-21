7d4bac917610a34be4f69dc7c2448ca2bb92d6b6
DAGIKE <isaac20053@hotmail.com>
2019-04-20 22:46:00 -0400

    config heroku for client

--- server/index.js
+++ server/index.js
@@ -26,5 +26,16 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  // Express serve up production assets
+  app.use(express.static("client/build"));
+
+  // Express serve up routes from index.html
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
