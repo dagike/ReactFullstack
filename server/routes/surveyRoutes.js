62bb5343207b3ffd453420c84de82dff45b6d42e
DAGIKE <isaac20053@hotmail.com>
2019-04-22 12:16:47 -0400

    implement survey route

--- server/routes/surveyRoutes.js
+++ server/routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const Mailer = require("../services/Mailer");
 const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 module.exports = app => {
-  app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -20,5 +20,16 @@ module.exports = app => {
     });
 
     const mailer = new Mailer(survey, surveyTemplate(survey));
+    try {
+      await mailer.send();
+
+      await survey.save();
+
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
