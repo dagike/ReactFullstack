9a5e72bf9bed5ffe7a15ab288d0c2046cce07c52
DAGIKE <isaac20053@hotmail.com>
2019-04-23 20:39:58 -0400

    add validation with regex for emails

--- server/client/src/components/surveys/SurveyForm.js
+++ server/client/src/components/surveys/SurveyForm.js
@@ -4,6 +4,7 @@ import { reduxForm, Field } from "redux-form";
 import { Link } from "react-router-dom";
 
 import SurveyField from "./SurveyField";
+import validateEmails from "../../utils/validateEmails";
 
 const FIELDS = [
   { label: "Survey Title", name: "title" },
@@ -44,6 +45,8 @@ class SurveyForm extends Component {
 
 const validate = values => {
   const errors = {};
+
+  errors.emails = validateEmails(values.emails || "");
 
   _.each(FIELDS, ({ name }) => {
     if (!values[name]) {
